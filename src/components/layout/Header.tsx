3d33be4432178c45ae7eb5c24695a099719e32a2
ayubaezekiel <ezekielayoba@yahoo.com>
2024-05-28 14:16:54 +0100

    animation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import ShimmerButton from "../ui/simmer-button";
 import { CaretDownIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Clock, MapPinIcon, PhoneCall } from "lucide-react";
 import { useScrollDetect } from "@/lib/utils";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import {
   DropdownMenu,
@@ -50,89 +50,144 @@ export const Header = () => {
   const { isScrolling } = useScrollDetect();
 
   return (
-    <header className="fixed z-50 w-full top-0 inset-x-0 bg-blue-900">
-      <AnimatePresence>
-        {!isScrolling && (
-          <motion.div
-            animate={{
-              opacity: 1,
-            }}
-            transition={{ delay: 0.03, ease: "easeInOut", type: "tween" }}
-            exit={{ opacity: 0 }}
-            className={`bg-white justify-between p-4 flex`}
-          >
-            <Link to={""}>
-              <img src={logo} alt="" className="w-48" />
-            </Link>
-            <div className="flex md:gap-10 gap-2">
-              <div className="text-xs flex md:flex-row flex-col items-center gap-1 font-serif">
-                <PhoneCall />
-                +2347049227730
-              </div>
-              <div className="text-xs flex flex-col md:flex-row items-center gap-1 font-serif">
-                <Clock /> 24/7
-              </div>
-              <div className="text-xs md:flex md:flex-row items-center hidden gap-1 font-serif">
-                <MapPinIcon /> Plot 1756 Cadastral zone E27 Apo Resettlement,
-                Abuja
-              </div>
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-
-      <div className="md:container flex items-center justify-between h-24">
-        <div className="flex justify-between w-full py-4 px-8 items-center">
-          <div className="md:hidden">
-            <Sheet>
-              <SheetTrigger>
-                <HamburgerMenuIcon className="stroke-white h-8 w-8 cursor-pointer" />
-              </SheetTrigger>
-              <SheetContent side={"top"}>
-                <div className="mt-10">
-                  <ul className="flex flex-col gap-4">
-                    {nav_links.map((l) =>
-                      l.title == "Services" ? (
-                        <li>
-                          <ServicesList />
-                        </li>
-                      ) : l.title == "Departments" ? (
-                        <DepartmentList />
-                      ) : (
-                        <li key={l.href} className="hover:underline">
-                          <Link to={l.href}>{l.title}</Link>
-                        </li>
-                      )
-                    )}
-                  </ul>
-                </div>
-              </SheetContent>
-            </Sheet>
+    <header className="fixed z-10 w-full top-0 inset-x-0 bg-transparent">
+      <motion.div
+        animate={{ opacity: isScrolling ? 0 : 1 }}
+        className={`justify-between p-4 flex bg-white`}
+      >
+        <Link to={""}>
+          <img src={logo} alt="" className="w-48" />
+        </Link>
+        <div className="flex md:gap-10 gap-2">
+          <div className="text-xs flex md:flex-row flex-col items-center gap-1 font-serif">
+            <PhoneCall />
+            +2347049227730
           </div>
-          <div className="hidden md:flex gap-10 justify-between items-center text-white">
-            <ul className="flex gap-4">
-              {nav_links.map((l) =>
-                l.title == "Services" ? (
-                  <li>
-                    <ServicesList />
-                  </li>
-                ) : l.title == "Departments" ? (
-                  <DepartmentList />
-                ) : (
-                  <li key={l.href} className="hover:underline">
-                    <Link to={l.href}>{l.title}</Link>
-                  </li>
-                )
-              )}
-            </ul>
+          <div className="text-xs flex flex-col md:flex-row items-center gap-1 font-serif">
+            <Clock /> 24/7
           </div>
-          <ShimmerButton className="shadow-2xl" background="none">
-            <span className="whitespace-pre-wrap text-center font-medium leading-none md:text-lg tracking-tight text-white dark:from-white dark:to-slate-900/10">
-              Appointment
-            </span>
-          </ShimmerButton>
+          <div className="text-xs md:flex md:flex-row items-center hidden gap-1 font-serif">
+            <MapPinIcon /> Plot 1756 Cadastral zone E27 Apo Resettlement, Abuja
+          </div>
         </div>
-      </div>
+      </motion.div>
+
+      {isScrolling ? (
+        <motion.div
+          animate={{
+            opacity: 1,
+            width: "100%",
+          }}
+          className="fixed top-0 bg-blue-900 flex items-center justify-between w-full"
+        >
+          <div className="flex justify-between w-full py-4 px-8 items-center">
+            <div className="md:hidden">
+              <Sheet>
+                <SheetTrigger>
+                  <HamburgerMenuIcon className="stroke-white h-8 w-8 cursor-pointer" />
+                </SheetTrigger>
+                <SheetContent side={"top"}>
+                  <div className="mt-10">
+                    <ul className="flex flex-col gap-4">
+                      {nav_links.map((l) =>
+                        l.title == "Services" ? (
+                          <li>
+                            <ServicesList />
+                          </li>
+                        ) : l.title == "Departments" ? (
+                          <DepartmentList />
+                        ) : (
+                          <li key={l.href} className="hover:underline">
+                            <Link to={l.href}>{l.title}</Link>
+                          </li>
+                        )
+                      )}
+                    </ul>
+                  </div>
+                </SheetContent>
+              </Sheet>
+            </div>
+            <div className="hidden md:flex gap-10 justify-between items-center text-white">
+              <ul className="flex gap-4">
+                {nav_links.map((l) =>
+                  l.title == "Services" ? (
+                    <li>
+                      <ServicesList />
+                    </li>
+                  ) : l.title == "Departments" ? (
+                    <DepartmentList />
+                  ) : (
+                    <li key={l.href} className="hover:underline">
+                      <Link to={l.href}>{l.title}</Link>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+            <ShimmerButton className="shadow-2xl" background="none">
+              <span className="whitespace-pre-wrap text-center font-medium leading-none md:text-lg tracking-tight text-white dark:from-white dark:to-slate-900/10">
+                Appointment
+              </span>
+            </ShimmerButton>
+          </div>
+        </motion.div>
+      ) : (
+        <motion.div
+          animate={{ opacity: 1, width: "100%" }}
+          className="flex items-center justify-between w-full"
+        >
+          <div className="flex justify-between w-full py-4 px-8 items-center bg-blue-900">
+            <div className="md:hidden">
+              <Sheet>
+                <SheetTrigger>
+                  <HamburgerMenuIcon className="stroke-white h-8 w-8 cursor-pointer" />
+                </SheetTrigger>
+                <SheetContent side={"top"}>
+                  <div className="mt-10">
+                    <ul className="flex flex-col gap-4">
+                      {nav_links.map((l) =>
+                        l.title == "Services" ? (
+                          <li>
+                            <ServicesList />
+                          </li>
+                        ) : l.title == "Departments" ? (
+                          <DepartmentList />
+                        ) : (
+                          <li key={l.href} className="hover:underline">
+                            <Link to={l.href}>{l.title}</Link>
+                          </li>
+                        )
+                      )}
+                    </ul>
+                  </div>
+                </SheetContent>
+              </Sheet>
+            </div>
+            <div className="hidden md:flex gap-10 justify-between items-center text-white">
+              <ul className="flex gap-4">
+                {nav_links.map((l) =>
+                  l.title == "Services" ? (
+                    <li>
+                      <ServicesList />
+                    </li>
+                  ) : l.title == "Departments" ? (
+                    <DepartmentList />
+                  ) : (
+                    <li key={l.href} className="hover:underline">
+                      <Link to={l.href}>{l.title}</Link>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+            <ShimmerButton className="shadow-2xl" background="none">
+              <span className="whitespace-pre-wrap text-center font-medium leading-none md:text-lg tracking-tight text-white dark:from-white dark:to-slate-900/10">
+                Appointment
+              </span>
+            </ShimmerButton>
+          </div>
+        </motion.div>
+      )}
     </header>
   );
 };
--- src/components/layout/NavbarWithBreadcrums.tsx
+++ src/components/layout/NavbarWithBreadcrums.tsx
@@ -7,31 +7,28 @@ import img1 from "../../assets/images/dialysis.jpg";
 import img2 from "../../assets/images/nicu.jpg";
 import img3 from "../../assets/images/privateicu.jpg";
 import { Button } from "../ui/button";
+import { useScrollDetect } from "@/lib/utils";
 
 const images = [img1, img2, img3];
 
 export const NavbarWithBreadcrums = () => {
+  const { isScrolling } = useScrollDetect();
   const loc = useLocation();
 
   const bread = loc.pathname.split("/").map((b) => b);
 
   return (
-    <div className="mt-[4rem]">
+    <motion.div
+      animate={{
+        marginTop: isScrolling ? 0 : 60,
+        opacity: 1,
+        y: 0,
+      }}
+      transition={{ ease: "easeInOut", type: "tween" }}
+      className=""
+    >
       <ImagesSlider className="h-[20rem]" images={images}>
-        <motion.div
-          initial={{
-            opacity: 0,
-            y: -80,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 0.6,
-          }}
-          className="z-50 flex flex-col justify-center items-center"
-        >
+        <div className="z-50 flex flex-col justify-center items-center">
           <div className="text-white my-4 pl-4 flex flex-col items-center justify-center">
             <h1 className="capitalize font-bold text-3xl">{bread}</h1>{" "}
             <div className="flex gap-2 capitalize justify-center">
@@ -41,8 +38,8 @@ export const NavbarWithBreadcrums = () => {
               <Button color="green">Book an Appointment</Button>
             </div>
           </div>
-        </motion.div>
+        </div>
       </ImagesSlider>
-    </div>
+    </motion.div>
   );
 };
